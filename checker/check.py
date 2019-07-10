77169659df7cd683aca5973a4eedff9cf903e4c7
OO7kartik <rohit.audrey@gmail.com>
2019-07-10 13:55:12 +0530

    Added Timelimit feature

--- checker/check.py
+++ checker/check.py
@@ -2,26 +2,57 @@
 
 import requests as req
 import bs4
+import subprocess
+import time
+from threading import Thread
 
-url = 'https://codeforces.com/problemset/submission/4/2785411'
+#needed stuff
+TimeLimit = 2 #time limit of the problem in seconds
+#should ask problem number and automatically crawel and then scrape
+
+class RunCmd(Thread):
+    def __init__(self, cmd, timeout):
+        Thread.__init__(self)
+        self.cmd = cmd
+        self.timeout = timeout
+
+    def run(self):
+        self.p = subprocess.Popen(self.cmd)
+        self.p.wait()
+
+    def Run(self):
+        self.start()
+        self.join(self.timeout)
+
+        if self.is_alive():
+            self.p.terminate()
+            self.join()
+            return True
+
+url = 'https://codeforces.com/problemset/submission/510/9680110'
 res = req.get(url)
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
 file = open('Files/TestCases.txt', 'w')
 i = 1
 for mydivs in soup.find_all("div", class_ = "file input-view"):
-    divs = mydivs.find('div', class_ = "text")
+    divs = mydivs.find('div', class_ = "text").get_text()
     file.write("case #" + str(i) + ":" + "\n")
-    file.write(divs.get_text())
+    if(divs[len(divs)-3] == '.'):
+        file.write("Long input....\n")
+    else:
+        file.write(divs)
     i+=1
-file.close()
 
 file = open('Files/Answers.txt', 'w')
 i = 1
 for mydivs in soup.find_all("div", class_ = "file answer-view"):
-    divs = mydivs.find('div', class_ = "text")
+    divs = mydivs.find('div', class_ = "text").get_text()
     file.write("case #" + str(i) + ":" + "\n")
-    file.write(divs.get_text())
+    if(divs[len(divs)-3] == '.'):
+        file.write("Long input....\n")
+    else:
+        file.write(divs)
     i+=1
 file.close()
 
@@ -29,31 +60,44 @@ file.close()
 #this runs your c++ code
 # generates its output and compares the output with the expected one
 
-import subprocess
-import time
-
 testcases = open('Files/TestCases.txt', 'r')
 input = open('Files/input.txt', 'w')
 output = open('Files/output.txt', 'w')
 output.close()
+results = open('Files/Result.txt','w')
+results.write("Grading in process....")
+results.close()
 
 #running and coolecting your c++ ouput
 i = 0
+LongInput = False
 for line in testcases:
-    if(line.find("case") == -1):
-        if(line != '\n'):
+    if(line == '\n'):
+        continue;
+    elif(line.find("case") == -1):
+        if("Long input...." in line):
+            LongInput = True
+        else:
             input.write(line)
     else:
         input.close()
         if(i > 0):
             output = open('Files/output.txt', 'a')
-            output.write("case #" + str(i) + ":" + "\n")
-            output.close()
-            subprocess.call(["g++", "test.cpp"])
-            subprocess.call("./a.exe")
-            output = open('Files/output.txt', 'a')
-            output.write("\n")
-            output.close()
+            if(LongInput):
+                output.write("case #" + str(i) + ":" + "\n")
+                output.write("Long input....\n")
+                LongInput = False
+            else:
+                output.write("case #" + str(i) + ":" + "\n")
+                output.close()
+                subprocess.call(["g++", "test.cpp"])
+                problem = RunCmd(["./a.exe"], TimeLimit).Run()
+                output = open('Files/output.txt', 'a')
+                if(problem):
+                    output.write("TLE\n")
+                else:
+                    output.write("\n")
+                output.close()
         input = open('Files/input.txt', 'w')
         i+=1
 
@@ -62,7 +106,11 @@ output.write("case #" + str(i) + ":" + "\n")
 output.close()
 input.close()
 subprocess.call(["g++", "test.cpp"])
-subprocess.call("./a.exe")
+problem = RunCmd(["./a.exe"], TimeLimit).Run()
+output = open('Files/output.txt', 'a')
+if(problem):
+    output.write("TLE")
+output.close()
 
 #now we have to check if the two files are samne
 output = open('Files/output.txt', 'r')
@@ -70,6 +118,11 @@ answer = open('Files/Answers.txt', 'r')
 result = open('Files/Result.txt', 'w')
 
 i = 0
+TotalCases = 0
+Passed = 0
+TimeLimit = 0
+Failed = 0
+Unjudjed = 0
 ans = True
 
 o_line = output.readline()
@@ -86,8 +139,12 @@ while(o_line and a_line):
     if("case" in o_line  and "case" in a_line):
         if(ans):
             result.write("case #" + str(i) + ": Passed\n\n")
+            Passed +=1
+            TotalCases += 1
         else:
-            result.write("case #" + str(i) + ": --------------------Failed\n\n")
+            result.write("case #" + str(i) + ": ---------Failed---------\n\n")
+            Failed += 1
+            TotalCases += 1
         i+=1
         ans = True
     elif("case" in o_line  or "case" in a_line):
@@ -96,6 +153,25 @@ while(o_line and a_line):
         while(a_line.find("case") == -1):
             a_line = answer.readline()
         ans = False
+    elif(not(o_line.find("Long input....") == -1)):
+        result.write("case #" + str(i) + ": >>>>Long input..Skipped\n\n")
+        i+=1
+        ans = True;
+        while(o_line and o_line.find("case") == -1):
+            o_line = output.readline()
+        while(a_line and a_line.find("case") == -1):
+            a_line = answer.readline()
+        Unjudjed += 1
+    elif(not(o_line.find("TLE") == -1)):
+        result.write("case #" + str(i) + ":  ---------TLE---------\n\n")
+        i+=1
+        ans = True;
+        while(o_line and o_line.find("case") == -1):
+            o_line = output.readline()
+        while(a_line and a_line.find("case") == -1):
+            a_line = answer.readline()
+        TimeLimit += 1
+        TotalCases += 1
     else:
         if(o_line != a_line):
             ans = False
@@ -106,5 +182,13 @@ while(o_line and a_line):
 output.close()
 answer.close()
 result.close()
+
+line = "AC: "+str(Passed)+"\nWA: "+str(Failed)+"\nTLE: "+str(TimeLimit)+"\nTotalCases: "+str(TotalCases)+"\n\nUnjudged due to long input:"+str(Unjudjed)+"\n\n\n" + "-----------------------------------------------------DETAILS---------------------------------------------------------------------\n\n\n"
+#to write this at the start of result.txt
+with open('Files/Result.txt', 'r+') as f:
+    content = f.read()
+    f.seek(0, 0)
+    f.write(line + '\n' + content)
+
 
 #check the results file to see how you performed
--- checker/test.cpp
+++ checker/test.cpp
@@ -1,30 +1,51 @@
-//file that checks working of watermelon :p
+//testing file :p
 
 #include <bits/stdc++.h>
 using namespace std;
+
+char mp[51][51];
+bool vis[51][51];
+int dx[4] = {
+	0, 0, 1, -1
+};
+int dy[4] = {
+  1, -1, 0, 0
+};
+int n, m;
+bool dfs(int x, int y, int px, int py, char c) {
+	vis[x][y] = 1;
+	for (int i = 0; i < 4; ++i) {
+		int tx = x + dx[i];
+		int ty = y + dy[i];
+		if (tx == px && ty == py) continue;
+		if (tx >= 0 && tx < n && ty >= 0 && ty < m && mp[tx][ty] == c) {
+			if (vis[tx][ty]) return 1;
+			if (dfs(tx, ty, x, y, c)) return 1;
+		}
+	}
+	return 0;
+}
+
 int main()
 {
 
     freopen("Files/input.txt", "r", stdin);
     freopen("Files/output.txt", "a", stdout);
 
-        long int kase,i,j,k,l,len,a,b;
-        char str[10000],str1[1000];
-        while(scanf("%ld",&kase)==1)
-        {
-        for(i=1;i<=kase;i++)
-        {
-            scanf("%s",&str);
-            len=strlen(str);
-            if(len<=10)
-            {
-                printf("%s\n",str);
-            }
-            else
-            {
-                printf("%c%d%c\n",str[0],len-2,str[len-1]);
-            }
-        }
-        return 0;
-    }
+    scanf("%d%d", &n, &m);
+  	for (int i = 0; i < n; ++i) {
+  		scanf(" %s", mp[i]);
+  	}
+  	for (int i = 0; i < n; ++i) {
+  		for (int j = 0; j < m; ++j) {
+  			if (!vis[i][j]) {
+  				if (dfs(i, j, -1, -1, mp[i][j])) {
+  					puts("Yes");
+  					return 0;
+  				}
+  			}
+  		}
+  	}
+  	puts("No");
+  	return 0;
 }
